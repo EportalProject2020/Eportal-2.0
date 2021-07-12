73d4e94466dee4f4eec4bab04f0dd62da9a41331
EportalProject2020 <EportalProject2020@gmail.com>
2021-07-13 01:39:41 +0200

    ok

--- app/src/main/java/com/project/eportal/IT/AddManager.java
+++ app/src/main/java/com/project/eportal/IT/AddManager.java
@@ -101,8 +101,6 @@ public class AddManager extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(AddManager.this, "Authentication done.",
-                                    Toast.LENGTH_SHORT).show();
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(AddManager.this, "Authentication failed.",
--- app/src/main/java/com/project/eportal/employee/EmployeeLogin.java
+++ app/src/main/java/com/project/eportal/employee/EmployeeLogin.java
@@ -1,47 +1,40 @@
 package com.project.eportal.employee;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.project.eportal.AdminActivity;
 import com.project.eportal.Forgetpassword;
 import com.project.eportal.IT.ITDashboard;
-import com.project.eportal.MainActivity;
 import com.project.eportal.R;
 import com.project.eportal.manager.ManagerDashboard;
-import com.project.eportal.manager.ManagerLogin;
 
 public class EmployeeLogin extends AppCompatActivity {
     private FirebaseFirestore database;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_login);
     }
 
-    @Override
-    public void onBackPressed() {
-        Intent intent = new Intent(EmployeeLogin.this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
 
     public void Login(View view) {
         FirebaseApp.initializeApp(this);
@@ -53,125 +46,112 @@ public class EmployeeLogin extends AppCompatActivity {
 
         String email = et_mail.getText().toString();
         String password = emp_pass.getText().toString();
-        if(!email.equals("")&&!password.equals("")) {
-            int y = copmaredata(email);
+        if (!email.equals("") && !password.equals("")) {
 
-            if (y != 0) {
-                if (y == 1) {
-                    Intent intent = new Intent(EmployeeLogin.this,
-                            AdminActivity.class);
-                    startActivity(intent);
+            authenticateuser(email, password);
 
-                } else if (y == 2) {
-                    Intent intent = new Intent(EmployeeLogin.this,
-                            Dashboard.class);
-                    startActivity(intent);
 
-                } else if (y == 3) {
-                    Intent intent = new Intent(EmployeeLogin.this,
-                            ITDashboard.class);
-                    startActivity(intent);
 
-                } else if (y == 4) {
-                    Intent intent = new Intent(EmployeeLogin.this,
-                            ManagerDashboard.class);
-                    startActivity(intent);
-
-                }
-            }
-        } 
-        else
+        } else
             Toast.makeText(this, "Please! Enter Email and Password", Toast.LENGTH_SHORT).show();
     }
-    int x=0;
-    private int copmaredata(final String email) {
-        if(x==0)
-        {
-            database.getInstance()
-                    .collection("Admin").document(email)
-                    .get()
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(EmployeeLogin.this, "no", Toast.LENGTH_SHORT).show();
 
-                        }
-                    })
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if(task.isSuccessful()) {
-                                x = 1;
-                            }else
-                                x=0;
-                        }
-                    });
-        }
-        if(x==0)
-        {
-            database.getInstance()
-                    .collection("users").document(email)
-                    .get()
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(EmployeeLogin.this, "no", Toast.LENGTH_SHORT).show();
+    int x = 0;
+    int y = 0;
 
-                        }
-                    })
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if(task.isSuccessful()) {
-                                x = 2;
-                            }else
-x=0;                        }
-                    });
-        }
-        if(x==0)
-        {
-            database.getInstance()
-                    .collection("IT").document(email)
-                    .get()
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(EmployeeLogin.this, "no", Toast.LENGTH_SHORT).show();
+    private int copmaredata(final String email, final String password) {
 
-                        }
-                    })
-                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if(task.isSuccessful()) {
-                                x = 3;
-                            }else
-x=0;                        }
-                    });
-        }
-     if(x==0)
-     {
-         database.getInstance()
-                 .collection("Managers").document(email)
-                 .get()
-                 .addOnFailureListener(new OnFailureListener() {
-                     @Override
-                     public void onFailure(@NonNull Exception e) {
-                         Toast.makeText(EmployeeLogin.this, "no", Toast.LENGTH_SHORT).show();
+        DocumentReference docRef = database.getInstance().collection("Admin").document(email);
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot snap, FirebaseFirestoreException fe) {
+                if (snap.exists()) {
+                    if (snap.get("mail").toString().equals(email) && snap.get("password").toString().equals(password)) {
 
-                     }
-                 })
-                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                     @Override
-                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                         if(task.isSuccessful()) {
-                             x = 4;
-                         }else
-x=0;                     }
-                 });
-     }
-       return x;
+                        Intent intent = new Intent(EmployeeLogin.this,
+                                AdminActivity.class);
+                        startActivity(intent);
+
+                    }
+                }
+            }
+
+        });
+        DocumentReference docRef1 = database.getInstance().collection("users").document(email);
+        docRef1.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot snap, FirebaseFirestoreException fe) {
+                if (snap.exists()) {
+                    if (snap.get("mail").toString().equals(email) && snap.get("password").toString().equals(password)) {
+                        {
+                            Intent intent = new Intent(EmployeeLogin.this,
+                                    Dashboard.class);
+                            startActivity(intent);
+                        }
+                    }
+                }
+            }
+
+        });
+        DocumentReference docRef2 = database.getInstance().collection("IT").document(email);
+        docRef2.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot snap, FirebaseFirestoreException fe) {
+                if (snap.exists()) {
+                    if (snap.get("mail").toString().equals(email) && snap.get("password").toString().equals(password)) {
+
+                        Intent intent = new Intent(EmployeeLogin.this,
+                                ITDashboard.class);
+                        startActivity(intent);
+                    }
+                    //update
+                }
+            }
+
+        });
+        DocumentReference docRef4 = database.getInstance().collection("Managers").document(email);
+        docRef4.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(DocumentSnapshot snap, FirebaseFirestoreException fe) {
+                if (snap.exists()) {
+                    if (snap.get("mail").toString().equals(email) && snap.get("password").toString().equals(password)) {
+
+                        Intent intent = new Intent(EmployeeLogin.this,
+                                ManagerDashboard.class);
+                        startActivity(intent);
+
+                    }
+                    //update
+                }
+            }
+
+        });
+
+        return x;
     }
+
+    private int authenticateuser(final String email, final String password) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        y=1;
+                        if (task.isSuccessful()) {
+                            // Sign in success,
+                            copmaredata(email, password);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(EmployeeLogin.this,
+                                    "Re-check the email and password you entered"
+                                    , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        return y;
+    }
+
+
     public void forgot_pass(View view) {
         Intent intent = new Intent(EmployeeLogin.this, Forgetpassword.class);
         startActivity(intent);
